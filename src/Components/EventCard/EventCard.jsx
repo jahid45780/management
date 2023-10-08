2fc21795fe053923bbccd966b0d2586d67c39ef3
jahid45780 <jahidhossain88899@gmail.com>
2023-10-09 05:24:17 +0600

    added the react icone

--- src/Components/EventCard/EventCard.jsx
+++ src/Components/EventCard/EventCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import { AiFillDollarCircle } from "react-icons/ai";
 
 const EventCard = ({ card }) => {
     const { id,logo, event_title, price, location, event_description } = card;
@@ -9,8 +9,8 @@ const EventCard = ({ card }) => {
             <img className=" w-96 h-72" src={logo} alt="" />
             <h2 className=" text-2xl font-bold" > {event_title} </h2>
             <div className=" flex gap-24 text-xl" >
-                <p> {location} </p>
-                <p>  {price} </p>
+                <p  >   {location} </p>
+                <p className=" flex gap-1 mr-1 items-center " > <AiFillDollarCircle className=" text-xl" ></AiFillDollarCircle> {price} </p>
             </div>
             <p> {event_description} </p>
 
